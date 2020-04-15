495ed2cb100c86d7d7a8b4656715c3ad518e9f81
bmqmingqi <baimingqi@qingmoyx.com>
2020-04-15 11:38:02 +0800

    配置文件修改

--- src/config/getui.php
+++ src/config/getui.php
@@ -9,37 +9,22 @@ return [
 
     'development' => [
         'client_1' => [
-            'gt_appid' => '87klYMPe1o515SCcbx7Co5',
-            'gt_appkey' => 'dd9XpsgHff89DJgUgvW6L8',
-            'gt_appsecret' => 'aKMLyeXLCc8hFpjcuf8gW8',
-            'gt_mastersecret' => 'zx85PndZVf8Q1M1Iv9dEy3',
+            'gt_appid' => 'XXXXXXXX',
+            'gt_appkey' => 'XXXXX',
+            'gt_appsecret' => 'XXXXX',
+            'gt_mastersecret' => 'XXXXXXXX',
             'gt_domainurl' => 'http://sdk.open.api.igexin.com/apiex.htm',
         ],
-        'client_2' => [
-            'gt_appid' => '87klYMPe1o515SCcbx7Co5',
-            'gt_appkey' => 'dd9XpsgHff89DJgUgvW6L8',
-            'gt_appsecret' => 'aKMLyeXLCc8hFpjcuf8gW8',
-            'gt_mastersecret' => 'zx85PndZVf8Q1M1Iv9dEy3',
-            'gt_domainurl' => 'http://sdk.open.api.igexin.com/apiex.htm',
-        ],
-
         // other client_3   ......
     ],
     'production' => [
         'client_1' => [
-            'gt_appid' => '87klYMPe1o515SCcbx7Co5',
-            'gt_appkey' => 'dd9XpsgHff89DJgUgvW6L8',
-            'gt_appsecret' => 'aKMLyeXLCc8hFpjcuf8gW8',
-            'gt_mastersecret' => 'zx85PndZVf8Q1M1Iv9dEy3',
+            'gt_appid' => 'XXXX',
+            'gt_appkey' => 'XXXX',
+            'gt_appsecret' => 'XXXX',
+            'gt_mastersecret' => 'XXXX',
             'gt_domainurl' => 'http://sdk.open.api.igexin.com/apiex.htm',
-        ],
-        'client_2' => [
-            'gt_appid' => '87klYMPe1o515SCcbx7Co5',
-            'gt_appkey' => 'dd9XpsgHff89DJgUgvW6L8',
-            'gt_appsecret' => 'aKMLyeXLCc8hFpjcuf8gW8',
-            'gt_mastersecret' => 'zx85PndZVf8Q1M1Iv9dEy3',
-            'gt_domainurl' => 'http://sdk.open.api.igexin.com/apiex.htm',
-        ],
+        ]
 
         // other client_3   ......
 
